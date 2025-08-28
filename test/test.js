879de944ea7de04e81ac131136573bc47873bd41
Dominic Szpak <149436108+Dszpak14@users.noreply.github.com>
2025-08-28 14:52:53 +0000

    c

--- test/test.js
+++ test/test.js
@@ -5,16 +5,10 @@ describe('Regular fibonacci value', function() {
         assert.strictEqual(main.fibonacci(5), 5);
       });
 });
+
 describe('First two terms', function() {
   it('should return 0 and 1 for first two terms', function() {
       assert.strictEqual(main.fibonacci(0), 0);
       assert.strictEqual(main.fibonacci(1), 1);
-    });
-}); 
-});
-describe('Negative number', function() {
-  it('should return undefined if negative', function() {
-      assert.strictEqual(main.fibonacci(-1), undefined);
-      assert.strictEqual(main.fibonacci(-10), undefined);
     });
 });
